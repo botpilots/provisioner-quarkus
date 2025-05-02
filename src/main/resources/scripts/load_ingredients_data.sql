56df19a24de7e5c6564480fb0f29fc9a3ae17724
hulsbo <oskar.huledal@simonsoft.se>
2025-05-02 17:50:06 +0200

    working db for ingredients.

---
+++ src/main/resources/scripts/load_ingredients_data.sql
@@ -0,0 +1,6 @@
+-- Sample ingredients
+INSERT INTO ingredients (name, protein_ratio, fat_ratio, carbs_ratio, fiber_ratio, salt_ratio, density_g_ml)
+VALUES
+    ('Olive Oil', 0.0000, 1.0000, 0.0000, 0.0000, 0.0000, 0.920),
+    ('Whole Wheat Flour', 0.1321, 0.0250, 0.7200, 0.1070, 0.0001, 0.593),
+    ('Salt', 0.0000, 0.0000, 0.0000, 0.0000, 1.0000, 2.160); 
